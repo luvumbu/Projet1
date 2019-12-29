75d19e66c92c00ad5817fbdd4dd42fe6ea6d5ce6
luvumbu <luvumbu.n@gmail.com>
2019-12-29 13:17:05 +0100

    ajout du package W3school pour les cours

---
+++ src/w3school/tuto/com/Cours.java
@@ -0,0 +1,25 @@
+package w3school.tuto.com;
+
+public class Cours {
+
+	public Cours() 
+	{
+		System.out.println("Bienvenue dans ce cours ");
+	}
+	public void lesson1() 
+	{
+		System.out.println("Lesson -1-");
+		System.out.println("Comment effectuer un commentaire");
+		System.out.println("1- commentaire sur une ligne  //");
+		System.out.println("2- commentaire sur plusieures lignes  /* PLACER VOTRE COMMENTAIRE ICI */ ");
+	}
+	public void lesson2() 
+	{
+		System.out.println("Lesson -2-");
+		System.out.println("Les variables ");
+		System.out.println("Il existent different type de variables ");
+		System.out.println("String , int , float , char , boolean");
+		System.out.println("String- texte magasins, tels que « Bonjour ». Les valeurs de chaîne sont entourées par des guillemets doubles");
+		System.out.println("int - entiers magasins (numéros entiers), sans décimales, tels que 123 ou -123");
+	}
+}
---
+++ src/w3school/tuto/com/Main.java
@@ -0,0 +1,13 @@
+package w3school.tuto.com;
+
+public class Main {
+
+	public static void main(String[] args) {
+	System.out.println("Je suis la classe principal");
+	Cours lesson1 = new Cours();
+	lesson1.lesson1();
+	
+
+	}
+
+}
